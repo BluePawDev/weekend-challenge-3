c32a5744a8aaf6df405b9e657f27784781a8d9e2
BluePawDev <jaruby@me.com>
2017-06-03 20:45:40 -0500

    Verify connection to db

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ var pg = require('pg');
 
 /*** GLOBALS ***/
 var app = express();
-var urlencodedParser = bodyParser.urlencoded({
-	extended: true
-});
 var config = {
 	database: 'dbToDo',
 	host: 'localhost',
@@ -16,40 +13,53 @@ var config = {
 	max: 12
 };
 var pool = new pg.Pool(config);
-var toDoArray = [];
+
 
 // Listen
 app.listen(7500, function() {
 	console.log('server listening on :7500');
 });
 
+
 /*** USES ***/
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 
 /*** ROUTES ***/
-// Base URL
+// START base URL
 app.get('/', function(req, res) {
 	console.log('Base URL hit');
 	res.sendFile(path.resolve('views/index.html'));
-});
+}); // END base URL
 
+
+// START tasks GET response
 app.get('/tasks', function(req, res) {
 	console.log('/tasks URL hit');
+	// Connect to dbToDo
 	pool.connect(function(err, connection, done) {
+		// Start if error
 		if (err) {
-			console.log('error in connection', err);
+			console.log('error connecting to db', err);
 			done();
 			res.send(400);
-		} else {
-			var toDoData = connection.query('SELECT * FROM tblToDo');
-
-			toDoData.on('row', function(row) {
+		} // End if error
+		else { // Start else
+			console.log('Connected to dbToDo');
+			var toDoArray = []; // Empty array for results
+			var resultSet = connection.query('SELECT * FROM "tblToDo"');
+			resultSet.on('row', function(row) {
+				// Loop through resultSet; push each row into toDoArray
 				toDoArray.push(row);
-			});
-			toDoArray.on('end', function() {
+			}); // End else
+			resultSet.on('end', function() {
+				// Close connection
 				done();
-				res.send(toDoData);
+				res.send(toDoArray);
 			});
 		}
 	});
-});
+}); // END tasks GET response
