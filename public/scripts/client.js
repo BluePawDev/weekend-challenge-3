583188661b4554d11f58343e100413b6e98ff20c
BluePawDev <jaruby@me.com>
2017-06-03 21:50:57 -0500

    Update getTasks()

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -26,10 +26,21 @@ var clickRemove = function() {
 // START tasks GET request
 function getTasks() {
 	$.ajax({
-		type: 'GET',
-		url: '/tasks',
-		success: function(response) {
-			console.log(response);
-		}
-	});
-} // END tasks GET request
+			type: 'GET',
+			url: '/tasks',
+			success: function(response) {
+				for (var i = 0; i < response.length; i++) {
+					var x = response[i];
+					console.log(response[i]);
+					var task = x.txtTask;
+					console.log(task);
+					var priority = x.txtPriority;
+					console.log(priority);
+					var due = x.dtmDue;
+					console.log(due);
+					var complete = x.dtmComplete;
+					$('#tblToDo').append('<tr id="' + i + '">' + '<td id="cellTask">' + '<input class="input txtTask" type="text" name="txtTask" value="' + task + '">' + '</td>' + '<td id = "cellPriority">' + '<input class="input inputCenter txtPriority" type="text" name="txtPriority" value="' + priority + '">' + '</td>' + '<td id="cellDue">' + '<input class="input inputCenter dtmDue" type="text" name="dtmDue" value="' + due + '">' + '</td>' + '<td id="cellComplete">' + '<input class="input inputCenter dtmDue" value disabled="disabled" type="text" name="dtmComplete" value="' + complete + '">' + '</td>' + '<td id="cellDone">' + '<button id="cmdDone" class="cmdBtn" type="button" name="cmdDone">' + 'Done' + '</button>' + '</td>' + '<td id="cellDone">' + '<button id="cmdRemove" class="cmdBtn" type="button" name="cmdRemove">' + 'Remove' + '</button>' + '</td>' + '</tr>);
+					}
+				}
+			});
+	} // END tasks GET request
